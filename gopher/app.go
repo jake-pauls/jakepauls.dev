84f7996514e8f01d8416f1520f21db5bc20a12ed
jacob-pauls <jakepauls99@gmail.com>
2021-04-22 17:43:33 -0700

    Added gin dep and made ping/pong test route

--- gopher/app.go
+++ gopher/app.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello Docker 🐋")
+	fmt.Println("🚀 Server running on https://localhost:8000")
+	app := gin.Default()
+
+	api := app.Group("/api")
+	{
+		api.GET("/ping", func(c *gin.Context) {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusOK, gin.H{
+				"ping": "pong",
+			})
+		})
+	}
+
+	app.Run(":4040")
 }
