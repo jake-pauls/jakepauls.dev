6a39b01479a5a8a54ac8d0a51c5bdbd2a31146d2
jacob-pauls <jakepauls99@gmail.com>
2021-05-10 01:55:22 -0700

    Added routes for profile, languages, and extended stats

--- gopher/routes/api/gh.go
+++ gopher/routes/api/gh.go
@@ -10,6 +10,15 @@ import (
 )
 
 const repos_url string = "https://api.github.com/users/jacob-pauls/repos"
+const profile_url string = "https://api.github.com/users/jacob-pauls"
+
+type Profile struct {
+    Name        string
+    Location    string
+    Followers   int
+    Following   int
+    URL         string  `json:"html_url"`
+}
 
 type License struct {
     Name        string
@@ -31,24 +40,83 @@ type Repository struct {
 
 // Register GitHub routers
 func GitHubRegister(group *gin.RouterGroup) {
+    group.GET("/profile", GetProfile)
     group.GET("/repos", GetRepos)
+    group.GET("/langs", GetLangs)
+    group.GET("/extended-stats", GetExtendedStats)
 }
 
-// Retrieve repository data
+func GetProfile(c *gin.Context) {
+    profile := GetProfileJSON()
+    c.JSON(http.StatusOK, profile)
+}
+
 func GetRepos(c *gin.Context) {
+    repositories := GetRepoJSON()
+    c.JSON(http.StatusOK, repositories)
+}
+
+// Derived from a repository's main language
+func GetLangs(c *gin.Context) {
+    repositories := GetRepoJSON()
+
+    languageMap := make(map[string]int)
+
+    for i := 0; i < len(repositories); i++ {
+        // Increment lang if it is already in map
+        if _, lang := languageMap[repositories[i].Language]; lang {
+            languageMap[repositories[i].Language]++
+            continue
+        }
+        languageMap[repositories[i].Language] = 1
+    }
+
+    c.JSON(http.StatusOK, languageMap)
+}
+
+// Stars and Watchers
+func GetExtendedStats(c *gin.Context) {
+    repositories := GetRepoJSON()
+
+    extendedStatsMap := make(map[string]int)
+
+
+    for i := 0; i < len(repositories); i++ {
+        extendedStatsMap["Watchers"] += repositories[i].Watchers
+        extendedStatsMap["Stars"] += repositories[i].Stars
+    }
+
+    c.JSON(http.StatusOK, extendedStatsMap)
+}
+
+func GetProfileJSON() Profile {
+    resp, err := http.Get(profile_url)
+
+    if err != nil {
+        zap.S().Infof("error retrieving profile data from github: %s", err)
+    }
+
+    defer resp.Body.Close()
+    body, _ := ioutil.ReadAll(resp.Body)
+
+    var profile Profile
+    json.Unmarshal([]byte(body), &profile)
+
+    return profile
+}
+
+func GetRepoJSON() []Repository {
     resp, err := http.Get(repos_url)
 
     if err != nil {
         zap.S().Infof("error retrieving repos from github: %s", err)
     }
 
-    // Close body and decode response
     defer resp.Body.Close()
     body, _ := ioutil.ReadAll(resp.Body)
 
-    // Parse JSON and append to array
     var repositories []Repository
     json.Unmarshal([]byte(body), &repositories)
 
-    c.JSON(http.StatusOK, repositories)
+    return repositories
 }
