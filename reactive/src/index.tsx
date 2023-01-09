424f6fe00c39af8e0b12f4a7e081191580f6c8b5
jake-pauls <jakepauls99@gmail.com>
2023-01-09 00:45:26 -0800

    Reverted to HashRouter after migrating to React-Router v6

--- reactive/src/index.tsx
+++ reactive/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 import { ModeProvider } from "./contexts/ModeContext";
@@ -10,9 +10,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ModeProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ModeProvider>
   </React.StrictMode>
 );
